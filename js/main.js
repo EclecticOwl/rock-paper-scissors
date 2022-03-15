3b7647998e6f6c8d63d62c696acb511e19538289
Michael Tillman <mnamllit@gmail.com>
2022-03-15 16:29:14 -0500

    added html ui elements

--- js/main.js
+++ js/main.js
@@ -14,31 +14,60 @@ let playerScore = 0;
 let computerScore = 0;
 let totalgames = 0;
 
+let p_score_div = document.getElementById('player-score')
+let p_score = document.createElement('p')
+p_score.textContent = 0
+
+let c_score_div = document.getElementById('computer-score')
+let c_score = document.createElement('p')
+c_score.textContent = 0
+
+let tie_score_div = document.getElementById('tie-score')
+let tie_score = document.createElement('p')
+tie_score.textContent = 0
+
+let total_score_div = document.getElementById('total-score')
+let total_score = document.createElement('p')
+total_score.textContent = 0
+
+
+
+
 let playRound = (playerSelection, computerSelection) => {
     let winner;
     if (playerSelection == computerSelection) {
         winner = 'It is a tie!';
+        tie_score.textContent++;
+        total_score.textContent++
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         winner = 'Player Wins!';
-        playerScore++;
+        p_score.textContent++;
+        total_score.textContent++;
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
         winner = 'Computer Wins!';;
-        computerScore++;
+        c_score.textContent++;
+        total_score.textContent++;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         winner = 'Computer Wins!';
-        computerScore++;
+        c_score.textContent++;
+        total_score.textContent++;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         winner = 'Player Wins!';
-        playerScore++;
+        p_score.textContent++;
+        total_score.textContent++;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         winner = 'Player Wins!';
-        playerScore++;
+        p_score.textContent++;
+        total_score.textContent++;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         winner = 'Computer Wins!';
-        computerScore++;
+        c_score.textContent++;
+        total_score.textContent++;
     }
     return winner;
 };
+
+
 
 rock.addEventListener('click', () => {
     game("rock");
@@ -51,16 +80,19 @@ scissors.addEventListener('click', () => {
 });
 
 
+let game_result = document.createElement('p')
 
 function game(player) {
-
     let computer = computerPlay()
-
     if (totalgames >= 10) {
         if ( playerScore > computerScore) {
-            console.log('Player Wins The Game!')
+            game_result.textContent = "Player wins the game!";
+            total_score_div.append(game_result)
+            
         }else {
-            console.log('Computer Wins The Game!')
+            game_result.textContent = "Computer wins the game!"
+            total_score_div.append(game_result)
+            
         }
     }else {
         console.log(playRound(player, computer))
@@ -71,12 +103,10 @@ function game(player) {
             Games: ${totalgames}
             `)
     }
-
-
-    
-
-    // console.log(playRound(player, computer));
 }
 
 
-
+p_score_div.append(p_score)
+c_score_div.append(c_score)
+tie_score_div.append(tie_score)
+total_score_div.append(total_score)
